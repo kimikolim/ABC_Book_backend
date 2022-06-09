a7216b2e442c6f38146ba55e7828a701afa047cb
Hakim Lim <hakimlim@Hakims-MacBook-Air-2.local>
2022-06-09 12:41:29 +0800

    mongodb database and collection setup

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
-import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
+import express, { Express, Request, Response } from "express"
+import mongoose from "mongoose"
 
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server")
 })
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+try {
+  mongoose.connect(
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.53dfgw3.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+  )
+  console.log("connected to database")
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `⚡️[server]: Server is running at https://localhost:${process.env.PORT}`
+    )
+  })
+} catch (error) {
+  console.log("mongodb connection failed:", error)
+}
